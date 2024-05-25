6058cd2bc4fe7b70d11ee1c1d6063c40ea8c29a2
Xikly <107074986+Xikly@users.noreply.github.com>
2024-05-25 23:50:36 +0100

    Main commit.
    Files for software.

---
+++ character.py
@@ -0,0 +1,43 @@
+import pyautogui
+
+class move():
+    '''
+    Move class used to interact with the player.
+    '''
+    def left() -> None:
+        '''
+        Moves the player left.
+        '''
+        pyautogui.keyDown("a")
+        
+    def right() -> None:
+        '''
+        Moves the player right.
+        '''
+        pyautogui.keyDown("d")
+    
+    def forward() -> None:
+        '''
+        Moves the player foward.
+        '''
+        pyautogui.keyDown("w")
+
+    def backward() -> None:
+        '''
+        Moves the player backward.
+        '''
+        pyautogui.keyDown("s")
+    
+    def stop(movement: str) -> None:
+        '''
+        Stops the player from moving using a given movement.
+
+        movement: str (The direction to stop)
+
+        Example: stop("right")
+        '''
+
+        if movement == "left": pyautogui.keyUp("a")
+        elif movement == "right": pyautogui.keyUp("d")
+        elif movement == "forward": pyautogui.keyUp("w")
+        elif movement == "backward": pyautogui.keyUp("s")
---
+++ main.py
@@ -0,0 +1,25 @@
+import character
+from tqdm import tqdm
+import time
+import random
+
+movements = [character.move.left, character.move.right, character.move.forward, character.move.backward]
+
+def single_movement():
+    previous_movement = ""
+
+    while True:
+        while True: # Ensures the movement previous movement isn't the same as the new one.
+            random_key = random.choice(movements)
+            if random_key.__name__ != previous_movement:
+                previous_movement = random_key.__name__
+                break
+        
+        random_key()
+
+        for x in tqdm(range(random.randint(3, 10))):
+            time.sleep(1)
+        
+        character.move.stop(random_key.__name__)
+
+single_movement()
