960b2fca21e6e55a0302aa2942467bfb8876be4e
Xikly <107074986+Xikly@users.noreply.github.com>
2024-05-31 05:10:42 +0100

    Added the ability for potentially 2 movements to run simultaneously
    This will roll a 1d2, if it lands on 2, two movements will be made, if it lands on 1, the default 1 movement route will be kept to.

--- main.py
+++ main.py
@@ -9,19 +9,36 @@ def single_movement():
     previous_movement = ""
 
     while True:
-        while True: # Ensures the movement previous movement isn't the same as the new one.
-            random_key = random.choice(movements)
-            if random_key.__name__ != previous_movement:
-                previous_movement = random_key.__name__
-                break
-        
-        random_key()
+        while True: # Ensures the movement previous movement isn't the same as the new one
+            multi = random.randint(1, 2)
 
-        for x in tqdm(range(random.randint(3, 10))):
-            time.sleep(1)
-            if random.randint(1, 10) == 1:
-                character.move.jump()
-        
-        character.move.stop(random_key.__name__)
+            if multi == 2:
+                random_key = [random.choice(movements), random.choice(movements)]
+
+                for key in random_key:
+                    key()
+                
+                for x in tqdm(range(random.randint(3, 10))):
+                    time.sleep(1)
+                    if random.randint(1, 10) == 1:
+                        character.move.jump()
+                
+                for key in random_key:
+                    character.move.stop(key.__name__)
+
+            else:
+                random_key = random.choice(movements)
+                if random_key.__name__ != previous_movement:
+                    previous_movement = random_key.__name__
+                    break
+
+                random_key()
+
+                for x in tqdm(range(random.randint(3, 10))):
+                    time.sleep(1)
+                    if random.randint(1, 10) == 1:
+                        character.move.jump()
+                
+                character.move.stop(random_key.__name__)
 
 single_movement()
